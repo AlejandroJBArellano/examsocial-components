eebbc130c834e4bb2a3c1e85642d6bf651e97ee7
Alejandro Arellano <jesusarellano334@gmail.com>
2025-01-21 17:27:04 -0600

    refactor: add BannerInput component for image upload functionality

---
+++ lib/components/BannerInput/BannerInput.tsx
@@ -0,0 +1,24 @@
+import PhotoIcon from "@mui/icons-material/Photo";
+
+const BannerInput = () => (
+  <label
+    htmlFor="banner"
+    className="xl:h-46 flex h-36 max-w-[358px] cursor-pointer items-center justify-center rounded-md border border-black bg-secondary p-4 xl:max-w-[640px]"
+  >
+    <div className="rounded-full border border-black bg-white p-2">
+      <PhotoIcon />
+    </div>
+    <input
+      id="banner"
+      type="file"
+      className="hidden"
+      accept="image/*"
+      multiple
+      onChange={(e) => {
+        console.log(e.target.files);
+      }}
+    />
+  </label>
+);
+
+export default BannerInput;

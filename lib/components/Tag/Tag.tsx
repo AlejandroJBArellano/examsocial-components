9319035e056f944e8463631e8f79157f219a678a
Alejandro Arellano <jesusarellano334@gmail.com>
2025-01-21 16:59:47 -0600

    feat: add Tag component for displaying labeled content

---
+++ lib/components/Tag/Tag.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+ const Tag = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="rounded-full border-sm border-feedback-success bg-feedback-success-tint px-2 py-1 text-xs xl:text-sm">
+      {children}
+    </div>
+  );
+};
+
+export default Tag;

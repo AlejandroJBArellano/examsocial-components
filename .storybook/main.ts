abcad69b0ee9328c6e89ba74a20805dc0157b160
Alejandro Arellano <jesusarellano334@gmail.com>
2025-01-24 08:09:37 -0600

    refactor: Storybook main configuration for improved structure and additional addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,16 @@
-export default {
-  stories: ["../lib/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
-  core: {
-    builder: "@storybook/builder-vite", // 👈 The builder enabled here.
-  },
-  framework: "@storybook/react-vite",
-  async viteFinal(config) {
-    // Merge custom configuration into the default config
-    const { mergeConfig } = await import("vite");
+import type { StorybookConfig } from "@storybook/react-vite";
 
-    return mergeConfig(config, {
-      // Add dependencies to pre-optimization
-      optimizeDeps: {
-        include: ["storybook-dark-mode"],
-      },
-    });
+const config: StorybookConfig = {
+  stories: ["../lib/components/**/*.stories.@(mdx|js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
 };
+export default config;

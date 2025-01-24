6442a06b31e0e03c7c22140a97fa93bc467f52a6
Alejandro Arellano <jesusarellano334@gmail.com>
2025-01-24 08:45:17 -0600

    feat: replace static avatar with ProfilePlaceholder component in Comment

--- lib/components/Comment/Comment.tsx
+++ lib/components/Comment/Comment.tsx
@@ -5,13 +5,14 @@ import {
   StarOutlineSharp,
 } from "@mui/icons-material";
 import { Button } from "../Button";
+import ProfilePlaceholder from "../ProfilePlaceholder/ProfilePlaceholder";
 
 const Comment = () => {
   return (
     <section className="space-y-2 mb-4" data-testid="comment">
       <article className="flex justify-between items-center">
         <div className="flex gap-1 items-center">
-          <div className="w-[22px] h-[22px] bg-blue-500 rounded-full" />
+          <ProfilePlaceholder gender="male" />
           <span className="text-base font-medium leading-5">John Doe</span>
         </div>
         <span className="text-xs">3 days ago</span>

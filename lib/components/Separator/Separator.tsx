8e2212b776da71d8d9dee10adeff1ffb9faa67ab
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-15 14:35:01 -0600

    feat: conditionally render text in Separator component

--- lib/components/Separator/Separator.tsx
+++ lib/components/Separator/Separator.tsx
@@ -3,10 +3,12 @@ import { PropsWithChildren } from "react";
 const Separator = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex gap-4 items-center">
-      <p className="text-lg md:text-[20px] leading-[20px] md:leading-[24px] font-medium md:tracking-[0.4px] tracking-[0.36px]">
-        {children}
-      </p>
-      <div className="flex w-full h-[1px] bg-black flex-auto"></div>
+      {children && (
+        <p className="text-lg md:text-[20px] leading-[20px] md:leading-[24px] font-medium md:tracking-[0.4px] tracking-[0.36px]">
+          {children}
+        </p>
+      )}
+      <div className="flex w-full h-[1px] bg-black flex-auto" />
     </div>
   );
 };

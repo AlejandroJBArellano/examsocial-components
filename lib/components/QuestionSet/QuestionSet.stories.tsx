f53f33190977f6507241c526ee94d4eb661d142f
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-15 13:24:47 -0600

    feat: add options to QuestionSet component with correct answer indication

--- lib/components/QuestionSet/QuestionSet.stories.tsx
+++ lib/components/QuestionSet/QuestionSet.stories.tsx
@@ -12,5 +12,23 @@ type Story = StoryObj<typeof QuestionSet>;
 export const Default: Story = {
   args: {
     question: "What is the capital of Nigeria?",
+    options: [
+      {
+        text: "Lagos",
+        correct: false,
+      },
+      {
+        text: "Abuja",
+        correct: true,
+      },
+      {
+        text: "Kano",
+        correct: false,
+      },
+      {
+        text: "Ibadan",
+        correct: false,
+      },
+    ],
   },
 };
--- lib/components/QuestionSet/QuestionSet.tsx
+++ lib/components/QuestionSet/QuestionSet.tsx
@@ -1,5 +1,6 @@
 import { Delete, Edit } from "@mui/icons-material";
 import { Button } from "../Button";
+import { Tag } from "../Tag";
 
 interface IQuestionSet {
   question: string;
@@ -9,17 +10,48 @@ interface IQuestionSet {
   }[];
   onDelete: (index: number) => void;
   onEdit: (index: number) => void;
+  index: number;
 }
 
-const QuestionSet = ({ question }: IQuestionSet) => {
+const QuestionSet = ({
+  question,
+  options,
+  onDelete,
+  onEdit,
+  index,
+}: IQuestionSet) => {
   return (
     <div className="p-4 space-y-4 border rounded-md border-black bg-extra-tint">
       <h4 className="text-xl leading-6 font-medium sentient">{question}</h4>
+      <div className="space-y-2.5">
+        {options.map((option, i) => (
+          <div key={i} className="flex justify-between items-center">
+            <p>{option.text}</p>
+            {option.correct ? (
+              <Tag theme="feedback-success">Correct</Tag>
+            ) : null}
+          </div>
+        ))}
+      </div>
       <div className="flex justify-between items-center">
-        <Button theme="feedback-error" rounded className="p-2">
+        <Button
+          onClick={() => {
+            onDelete(index);
+          }}
+          theme="feedback-error"
+          rounded
+          className="p-2"
+        >
           <Delete className="!w-8 !h-8 xl:!w-9 xl:!h-9" />
         </Button>
-        <Button theme="light" rounded className="p-2">
+        <Button
+          onClick={() => {
+            onEdit(index);
+          }}
+          theme="light"
+          rounded
+          className="p-2"
+        >
           <Edit className="!w-8 !h-8 xl:!w-9 xl:!h-9" />
         </Button>
       </div>

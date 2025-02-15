39e6ec29690e2d30357a60bb48f26792aa1ba5e8
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-15 14:35:34 -0600

    feat: import react-material-symbols/rounded at index and utils

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
+import "react-material-symbols/rounded";
 import "./index.css";
 
 export * from "./components/index";
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import clsx, { ClassValue } from "clsx";
+import "react-material-symbols/rounded";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {

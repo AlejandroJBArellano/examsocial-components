444cceb7ec68ac512b095853c33c50048946312d
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-15 12:37:32 -0600

    feat: add title, time and showDivision at Stepper

--- lib/components/Stepper/Stepper.tsx
+++ lib/components/Stepper/Stepper.tsx
@@ -6,25 +6,33 @@ interface IStepperProps {
   activeStep: number;
   steps: number;
   onClickStep: (step: number) => void;
+  title: string;
+  time: string;
+  showDivision?: boolean;
 }
 
 const Stepper = ({
   steps,
   activeStep,
   onClickStep,
+  title,
+  time,
+  showDivision,
 }: PropsWithChildren<IStepperProps>) => {
   return (
     <section className="bg-primary-tint border-b-sm sentient border-b-gray-500 p-4 pt-3 space-y-3">
       <article className="items-center flex justify-between">
         <p className="text-[32px] sentient font-medium tracking-[0.64px] leading-10 text-primary-shadow">
-          Stepper
+          {title}
         </p>
-        <div className="flex gap-2 items-center">
-          <p className="text-[28px] sentient font-[inherit] font-bold leading-8 tracking-[0.56px]">
-            20:00
-          </p>
-          <Timer />
-        </div>
+        {time ? (
+          <div className="flex gap-2 items-center">
+            <p className="text-[28px] sentient font-[inherit] font-bold leading-8 tracking-[0.56px]">
+              20:00
+            </p>
+            <Timer />
+          </div>
+        ) : null}
       </article>
       <article className="items-center flex justify-between">
         <div className="flex items-center gap-3">
@@ -41,9 +49,11 @@ const Stepper = ({
             </Button>
           ))}
         </div>
-        <span className="text-xl leading-[24px] sentient font-medium tracking-[0.4px]">
-          {activeStep}/{steps}
-        </span>
+        {showDivision ? (
+          <span className="text-xl leading-[24px] sentient font-medium tracking-[0.4px]">
+            {activeStep}/{steps}
+          </span>
+        ) : null}
       </article>
     </section>
   );

633b69b4997dee759cf93d89865cd0ca9555088e
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-15 15:29:59 -0600

    feat: implement EditQuestion component with onSubmit and onCancel handlers

--- lib/components/EditQuestion/EditQuestion.tsx
+++ lib/components/EditQuestion/EditQuestion.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../Button";
 import { QuestionForm } from "../QuestionForm";
 
@@ -6,16 +7,33 @@ interface IValues {
   answers: { text: string; correct: boolean }[];
 }
 
-const EditQuestion = (initialValues: IValues) => {
+interface IEditQuestion {
+  onSubmit: (values: IValues) => void;
+  onCancel: () => void;
+  initialValues: IValues;
+}
+
+const EditQuestion = ({ initialValues, onSubmit, onCancel }: IEditQuestion) => {
+  const [values, setValues] = useState<IValues | null>(null);
   return (
     <div className="p-4 shadow-right shadow-black border border-black rounded-lg space-y-6">
       <h2 className="text-2xl leading-7 sentient">Edit Question</h2>
-      <QuestionForm onChange={() => {}} initialValues={initialValues} />
+      <QuestionForm
+        onChange={(newValues) => {
+          setValues(newValues);
+        }}
+        initialValues={initialValues}
+      />
       <div className="flex justify-between items-center">
-        <Button theme="light" rounded>
+        <Button theme="light" rounded onClick={onCancel}>
           Cancel
         </Button>
-        <Button rounded theme="accent">
+        <Button
+          rounded
+          theme="accent"
+          disabled={!values}
+          onClick={() => values && onSubmit(values)}
+        >
           Update
         </Button>
       </div>

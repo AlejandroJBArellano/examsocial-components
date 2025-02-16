32ef8244eb6c313ce274143f2c2e5d89ee2e964c
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-15 20:01:00 -0600

    feat: implement modal dialog for adding new questions in CreateExam component

--- lib/components/CreateExam/Questions.tsx
+++ lib/components/CreateExam/Questions.tsx
@@ -1,14 +1,20 @@
 import { Add } from "@mui/icons-material";
+import { useRef } from "react";
 import { Button } from "../Button";
+import { NewQuestion } from "../NewQuestion";
 
 export const Questions = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   return (
     <section className="space-y-6">
       <h2 className="sentient font-bold text-[28px] leading-8 tracking-[0.56px]">
         Questions
       </h2>
-      <article></article>
+      <article className="h-screen"></article>
       <Button
+        onClick={() => {
+          dialogRef.current?.showModal();
+        }}
         theme="extra"
         rounded
         className="w-full items-center flex gap-2 justify-center"
@@ -16,6 +22,17 @@ export const Questions = () => {
         <Add className="!w-5 !h-5" />
         <span className="font-medium">Add new question</span>
       </Button>
+      <dialog
+        ref={dialogRef}
+        className="backdrop:bg-black/50 rounded-lg backdrop:backdrop-blur-md"
+      >
+        <NewQuestion
+          onSubmit={() => {}}
+          onCancel={() => {
+            dialogRef.current?.close();
+          }}
+        />
+      </dialog>
     </section>
   );
 };

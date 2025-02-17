2f2d8cd1e30b774ddc98d18dd7c218fe40161f53
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-16 20:45:44 -0600

    feat: update Review component layout with improved headings for better content organization

--- lib/components/CreateExam/Review.tsx
+++ lib/components/CreateExam/Review.tsx
@@ -5,9 +5,9 @@ export const Review = () => {
   return (
     <div className="space-y-4">
       <div className="flex justify-between items-center">
-        <h4 className="text-2xl leading-7 font-medium tracking-[0.48px]">
+        <h2 className="sentient font-bold text-[28px] leading-8 tracking-[0.56px]">
           Review
-        </h4>
+        </h2>
         <Button
           theme="extra"
           className="flex items-center justify-center gap-2"
@@ -16,6 +16,18 @@ export const Review = () => {
           <span className="font-medium">Preview exam</span>
         </Button>
       </div>
+      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
+        General Details
+      </h3>
+      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
+        Questions
+      </h3>
+      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
+        Additional Content
+      </h3>
+      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
+        Advanced Settings
+      </h3>
     </div>
   );
 };

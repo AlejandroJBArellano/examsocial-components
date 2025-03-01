56d1e984a60a8e585ddb01a2b87cf9a03cb0f0d9
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-01 11:52:16 -0600

    feat: add total time controls to TimingSettings component for improved timing configuration

--- lib/components/TimingSettings/TimingSettings.tsx
+++ lib/components/TimingSettings/TimingSettings.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FocusSpan, Smoll } from "../FontFaces";
+import { Input } from "../Input";
 import { Select } from "../Select";
 
 export type TimingSetting = "NONE" | "TOTAL" | "PER_QUESTION" | "CUSTOM";
@@ -20,47 +21,80 @@ export const TimingSettingsNameMap = {
 
 const TimingSettings = () => {
   const [timingSetting, setTimingSetting] = useState<TimingSetting>("NONE");
-  return (
-    <article className="space-y-1">
-      <div className="flex justify-between items-center">
-        <FocusSpan>Timing</FocusSpan>
-        <div className="w-1/2">
-          <Select text={TimingSettingsNameMap[timingSetting]}>
-            <Select.Option
-              onClick={() => {
-                setTimingSetting("NONE");
-              }}
-            >
-              None
-            </Select.Option>
-            <Select.Option
-              onClick={() => {
-                setTimingSetting("TOTAL");
-              }}
-            >
-              Total
-            </Select.Option>
-            <Select.Option
-              onClick={() => {
-                setTimingSetting("PER_QUESTION");
-              }}
-            >
-              Per Question
-            </Select.Option>
-            <Select.Option
-              onClick={() => {
-                setTimingSetting("CUSTOM");
-              }}
-            >
-              Custom
-            </Select.Option>
-          </Select>
+
+  const TotalTimeControls = {
+    NONE: null,
+    CUSTOM: null,
+    TOTAL: (
+      <article className="flex items-center gap-4 w-full">
+        <div className="space-y-1 w-1/2">
+          <FocusSpan>Hours</FocusSpan>
+          <Input type="number" placeholder="0" className="w-full" />
         </div>
-      </div>
-      <Smoll className="text-end block">
-        {TimingSettingsDescriptionMap[timingSetting]}
-      </Smoll>
-    </article>
+        <div className="space-y-1 w-1/2">
+          <FocusSpan>Minutes</FocusSpan>
+          <Input type="number" placeholder="0" className="w-full" />
+        </div>
+      </article>
+    ),
+    PER_QUESTION: (
+      <article className="flex items-center gap-4 w-full">
+        <div className="space-y-1 w-1/2">
+          <FocusSpan>Minutes</FocusSpan>
+          <Input type="number" placeholder="0" className="w-full" />
+        </div>
+        <div className="space-y-1 w-1/2">
+          <FocusSpan>Seconds</FocusSpan>
+          <Input type="number" placeholder="0" className="w-full" />
+        </div>
+      </article>
+    ),
+  };
+
+  return (
+    <section className="space-y-4">
+      <article className="space-y-1">
+        <div className="flex justify-between items-center">
+          <FocusSpan>Timing</FocusSpan>
+          <div className="w-1/2">
+            <Select text={TimingSettingsNameMap[timingSetting]}>
+              <Select.Option
+                onClick={() => {
+                  setTimingSetting("NONE");
+                }}
+              >
+                None
+              </Select.Option>
+              <Select.Option
+                onClick={() => {
+                  setTimingSetting("TOTAL");
+                }}
+              >
+                Total
+              </Select.Option>
+              <Select.Option
+                onClick={() => {
+                  setTimingSetting("PER_QUESTION");
+                }}
+              >
+                Per Question
+              </Select.Option>
+              <Select.Option
+                onClick={() => {
+                  setTimingSetting("CUSTOM");
+                }}
+              >
+                Custom
+              </Select.Option>
+            </Select>
+          </div>
+        </div>
+        <Smoll className="text-end block">
+          {TimingSettingsDescriptionMap[timingSetting]}
+        </Smoll>
+      </article>
+      {TotalTimeControls[timingSetting]}
+    </section>
   );
 };
 

b40fc488ba8d3b4d2d363cd82b43dd1acca2664e
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-02 08:02:18 -0600

    feat: implement form handling and validation in FeedbackScreen component

--- lib/components/FeedbackScreen/New.tsx
+++ lib/components/FeedbackScreen/New.tsx
@@ -1,4 +1,6 @@
+import { useFormik, useFormikContext } from "formik";
 import { useEffect, useState } from "react";
+import * as Yup from "yup";
 import { Button } from "../Button";
 import { Input } from "../Input";
 import { Select } from "../Select";
@@ -12,29 +14,87 @@ const FeedbackCondition = {
   LESS_THAN: "Less than",
 };
 
-const FeedbackConditionNodes = {
-  [FeedbackCondition.ALL]: null,
-  [FeedbackCondition.BETWEEN]: (
-    <div className="flex items-center gap-2">
-      <Input placeholder="0" type="number" className="w-full h-11" />
-      <span className="text-lg leading-6">and</span>
-      <Input placeholder="100" type="number" className="w-full h-11" />
-    </div>
+const validationSchema = Yup.object({
+  message: Yup.string().required("Message is required"),
+  condition: Yup.string()
+    .oneOf(Object.values(FeedbackCondition))
+    .required("Condition is required"),
+  min: Yup.number().when("condition", (condition, schema) =>
+    condition[0] === FeedbackCondition.BETWEEN
+      ? schema.required("Minimum value is required")
+      : schema,
   ),
-  [FeedbackCondition.EQUAL_TO]: (
-    <Input placeholder="100" className="w-full h-11" type="number" />
+  max: Yup.number().when("condition", (condition, schema) =>
+    condition[0] === FeedbackCondition.BETWEEN
+      ? schema.required("Maximum value is required")
+      : schema,
   ),
-  [FeedbackCondition.GREATER_THAN]: (
-    <Input placeholder="100" className="w-full h-11" type="number" />
+  equal: Yup.number().when("condition", (condition, schema) =>
+    condition[0] === FeedbackCondition.EQUAL_TO
+      ? schema.required("Equal value is required")
+      : schema,
   ),
-  [FeedbackCondition.LESS_THAN]: (
-    <Input placeholder="100" className="w-full h-11" type="number" />
+  gt: Yup.number().when("condition", (condition, schema) =>
+    condition[0] === FeedbackCondition.GREATER_THAN
+      ? schema.required("Greater than value is required")
+      : schema,
   ),
-};
+  lt: Yup.number().when("condition", (condition, schema) =>
+    condition[0] === FeedbackCondition.LESS_THAN
+      ? schema.required("Less than value is required")
+      : schema,
+  ),
+});
 
 const FeedbackConditionField = () => {
-  const [condition, setCondition] = useState(FeedbackCondition.ALL);
   const [container, setContainer] = useState<HTMLElement>();
+
+  const formik = useFormikContext<Yup.InferType<typeof validationSchema>>();
+
+  const FeedbackConditionNodes = {
+    [FeedbackCondition.ALL]: null,
+    [FeedbackCondition.BETWEEN]: (
+      <div className="flex items-center gap-2">
+        <Input
+          placeholder="0"
+          type="number"
+          className="w-full h-11"
+          {...formik.getFieldProps("min")}
+        />
+        <span className="text-lg leading-6">and</span>
+        <Input
+          placeholder="100"
+          type="number"
+          className="w-full h-11"
+          {...formik.getFieldProps("max")}
+        />
+      </div>
+    ),
+    [FeedbackCondition.EQUAL_TO]: (
+      <Input
+        placeholder="100"
+        className="w-full h-11"
+        type="number"
+        {...formik.getFieldProps("equal")}
+      />
+    ),
+    [FeedbackCondition.GREATER_THAN]: (
+      <Input
+        placeholder="100"
+        className="w-full h-11"
+        type="number"
+        {...formik.getFieldProps("gt")}
+      />
+    ),
+    [FeedbackCondition.LESS_THAN]: (
+      <Input
+        placeholder="100"
+        className="w-full h-11"
+        type="number"
+        {...formik.getFieldProps("lt")}
+      />
+    ),
+  };
 
   useEffect(() => {
     const containerDOM = document.getElementById("advanced-settings");
@@ -47,19 +107,22 @@ const FeedbackConditionField = () => {
     <article className="grid grid-cols-2 gap-4 items-end">
       <div className="space-y-1">
         <label className="font-medium">Condition</label>
-        <Select text={condition || "Select one"} container={container}>
-          {Object.entries(FeedbackCondition).map(([key, value]) => (
+        <Select
+          text={formik.values.condition || "Select one"}
+          container={container}
+        >
+          {Object.entries(FeedbackCondition).map(([key, conditionValue]) => (
             <Select.Option
               key={key}
-              checked={condition === value}
-              onClick={() => setCondition(value)}
+              checked={formik.values.condition === conditionValue}
+              onClick={() => formik.setFieldValue("condition", conditionValue)}
             >
-              {value}
+              {conditionValue}
             </Select.Option>
           ))}
         </Select>
       </div>
-      <div>{FeedbackConditionNodes[condition]}</div>
+      <div>{FeedbackConditionNodes[formik.values.condition]}</div>
     </article>
   );
 };
@@ -75,6 +138,17 @@ interface INewFeedbackScreen {
 }
 
 const NewFeedbackScreen = ({ onSubmit, onCancel }: INewFeedbackScreen) => {
+  const formik = useFormik({
+    initialValues: {
+      message: "",
+      condition: FeedbackCondition.ALL,
+    },
+    onSubmit: (values) => {
+      onSubmit(values);
+    },
+    validationSchema,
+  });
+
   return (
     <div className="p-4 border rounded-lg shadow-right-sm shadow-black border-black space-y-6">
       <h4 className="text-2xl leading-7 tracking-[0.48px] font-medium sentient">
@@ -86,6 +160,9 @@ const NewFeedbackScreen = ({ onSubmit, onCancel }: INewFeedbackScreen) => {
           <Textarea
             className="w-full p-2 border rounded-lg"
             placeholder="e.g., Congrats, you got a perfect score!"
+            name="message"
+            value={formik.values.message}
+            onChange={formik.handleChange}
           />
         </article>
         <FeedbackConditionField />
@@ -94,7 +171,7 @@ const NewFeedbackScreen = ({ onSubmit, onCancel }: INewFeedbackScreen) => {
         <Button rounded onClick={onCancel}>
           Cancel
         </Button>
-        <Button theme="accent" rounded>
+        <Button theme="accent" rounded onClick={formik.submitForm}>
           Save
         </Button>
       </div>

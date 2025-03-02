092779bec62c8f2ac6ed6223c9c07e7862234a7f
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-02 10:40:05 -0600

    feat: integrate ExamCard into Review component and enhance layout with formik context

--- lib/components/CreateExam/Review.tsx
+++ lib/components/CreateExam/Review.tsx
@@ -1,33 +1,38 @@
 import { Visibility } from "@mui/icons-material";
+import { useFormikContext } from "formik";
+import * as Yup from "yup";
+import { examSchema } from "../../schemas/index";
 import { Button } from "../Button";
+import ExamCard from "../ExamCard/ExamCard";
+import { FocusSpan, Heading3, Heading4 } from "../FontFaces";
 
 export const Review = () => {
+  const formik = useFormikContext<Yup.InferType<typeof examSchema>>();
   return (
-    <div className="space-y-4">
-      <div className="flex justify-between items-center">
-        <h2 className="sentient font-bold text-[28px] leading-8 tracking-[0.56px]">
-          Review
-        </h2>
+    <section className="space-y-4">
+      <article className="flex justify-between items-center">
+        <Heading3>Review</Heading3>
         <Button
           theme="extra"
           className="flex items-center justify-center gap-2"
         >
           <Visibility className="!w-5 !h-5" />
-          <span className="font-medium">Preview exam</span>
+          <FocusSpan>Preview exam</FocusSpan>
         </Button>
-      </div>
-      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
-        General Details
-      </h3>
-      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
-        Questions
-      </h3>
-      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
-        Additional Content
-      </h3>
-      <h3 className="sentient font-medium text-2xl leading-7 tracking-[0.48px]">
-        Advanced Settings
-      </h3>
-    </div>
+      </article>
+      <article className="space-y-10">
+        <Heading4>General Details</Heading4>
+        <ExamCard
+          title={formik.values.title}
+          description={formik.values.description}
+          tag="XD"
+          time="10 min"
+          image="https://placecats.com/300/200"
+        />
+      </article>
+      <Heading4>Questions</Heading4>
+      <Heading4>Additional Content</Heading4>
+      <Heading4>Advanced Settings</Heading4>
+    </section>
   );
 };

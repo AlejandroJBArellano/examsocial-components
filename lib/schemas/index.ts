b1f82171299ba50a2b15f5967d24af51b7a1d0b6
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-02 08:08:54 -0600

    feat: add validation schema for advanced settings

--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -26,3 +26,21 @@ export const examSchema = Yup.object({
     .required()
     .min(1, "At least one question is required"),
 });
+
+export const advancedSettingsSchema = Yup.object({
+  numberOfAttempts: Yup.number()
+    .min(1, "Must be at least 1")
+    .required("Required"),
+  price: Yup.number().min(0, "Must be at least 0").required("Required"),
+  privacy: Yup.object({
+    setting: Yup.string().required("Required"),
+    invitees: Yup.array().when("setting", {
+      is: (val: string) => val === "INVITE_ONLY",
+      then: (schema) => schema.of(Yup.string().email("Invalid email")),
+    }),
+    password: Yup.string().when("setting", {
+      is: (val: string) => val === "PASSWORD",
+      then: (schema) => schema.required("Required"),
+    }),
+  }),
+});

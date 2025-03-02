d06f63016ee24528abc414b3325344876557444d
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-02 11:25:45 -0600

    feat: add content type selection to NewAdditionalContent component

--- lib/components/AdditionalContent/New.tsx
+++ lib/components/AdditionalContent/New.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { Button } from "../Button";
 import { FocusSpan, Heading4 } from "../FontFaces";
 import { Input } from "../Input";
+import { Select } from "../Select";
 import { Textarea } from "../Textarea";
 
 interface INewAdditionalContent {
@@ -12,9 +14,26 @@ export const NewAdditionalContent = ({
   onSubmit,
   onCancel,
 }: INewAdditionalContent) => {
+  const [container, setContainer] = useState<HTMLElement>();
+
+  useEffect(() => {
+    const containerDOM = document.getElementById("additional-content");
+    if (containerDOM) {
+      setContainer(containerDOM);
+    }
+  }, []);
   return (
     <section className="p-4 shadow-right rounded-lg shadow-black border border-black space-y-6">
       <Heading4>New Content</Heading4>
+      <Select text="Content Type" container={container}>
+        <Select.Option>YouTube URL</Select.Option>
+        <Select.Option>Link</Select.Option>
+        <Select.Option>Text</Select.Option>
+        <Select.Option>Image</Select.Option>
+        <Select.Option>Video</Select.Option>
+        <Select.Option>Audio</Select.Option>
+        <Select.Option>File</Select.Option>
+      </Select>
       <article className="space-y-4">
         <div className="space-y-1">
           <FocusSpan>YouTube URL</FocusSpan>

c9ac40167f10295b48c55edc26e8bf1c6ae8d7be
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-02 11:50:46 -0600

    feat: add ImageUploader component for displaying uploaded images

---
+++ lib/components/ImageUploader/ImageUploader.tsx
@@ -0,0 +1,19 @@
+import { X } from "@mui/icons-material";
+import { Smoll, Span } from "../FontFaces";
+
+const ImageUploader = ({ image }: { image: File }) => {
+  return (
+    <article className="p-2 flex items-center justify-between bg-primary-tint border-sm border-primary-shadow">
+      <div className="gap-2 flex items-center">
+        <img className="w-[81px] h-10" src={URL.createObjectURL(image)} />
+        <div className="space-y-1">
+          <Span className="block text-primary-shadow">{image.name}</Span>
+          <Smoll>{image.size} mb.</Smoll>
+        </div>
+      </div>
+      <X className="!w-6 !h-6 text-primary" />
+    </article>
+  );
+};
+
+export default ImageUploader;

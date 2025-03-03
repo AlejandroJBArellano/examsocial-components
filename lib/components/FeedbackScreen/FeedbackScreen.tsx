8b2b32b05fa5e8bcdb33cbc4a63ff0af810538c2
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-03 17:24:21 -0600

    feat: replace title and description elements with FocusSpan and Paragraph in FeedbackScreen component

--- lib/components/FeedbackScreen/FeedbackScreen.tsx
+++ lib/components/FeedbackScreen/FeedbackScreen.tsx
@@ -1,5 +1,6 @@
 import { Delete, Edit } from "@mui/icons-material";
 import { Button } from "../Button";
+import { FocusSpan, Paragraph } from "../FontFaces";
 
 interface IFeedbackScreen {
   title: string;
@@ -9,10 +10,8 @@ interface IFeedbackScreen {
 const FeedbackScreen = ({ title, description }: IFeedbackScreen) => {
   return (
     <div className="p-4 space-y-4 border border-black rounded-md xl:space-y-5 xl:p-6">
-      <h4 className="font-medium leading-5 text-base xl:text-lg xl:leading-6">
-        {title}
-      </h4>
-      <p className="font-light xl:text-lg">{description}</p>
+      <FocusSpan>{title}</FocusSpan>
+      <Paragraph>{description}</Paragraph>
       <div className="flex items-center justify-between">
         <Button theme="feedback-error" rounded className="p-2 xl:p-2">
           <Delete className="!w-8 !h-8 xl:!w-9 xl:!h-9" />

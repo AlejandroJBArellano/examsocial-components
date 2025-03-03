d38b68fa5abd9de2ccf782d547db133c755c199f
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-03 17:44:59 -0600

    feat: add handleCondition function to process feedback conditions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,31 @@
 import clsx, { ClassValue } from "clsx";
 import "react-material-symbols/rounded";
 import { twMerge } from "tailwind-merge";
+import * as Yup from "yup";
+import { feedbackSchema } from "./schemas";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const regexYouTubeURL = new RegExp(
-  /^(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+$/
+  /^(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+$/,
 );
+
+export const handleCondition = (
+  feedback: Yup.InferType<typeof feedbackSchema>,
+) => {
+  if (feedback.condition[0] === "EQUAL_TO") {
+    return `Equal to ${feedback.equal}`;
+  }
+  if (feedback.condition[0] === "GREATER_THAN") {
+    return `More than ${feedback.gt}`;
+  }
+  if (feedback.condition[0] === "LESS_THAN") {
+    return `Less than ${feedback.lt}`;
+  }
+  if (feedback.condition[0] === "BETWEEN") {
+    return `Between ${feedback.min} and ${feedback.max}`;
+  }
+  return "All";
+};

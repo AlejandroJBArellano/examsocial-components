545892616d08a22cf24b3a7d470e4e17bd7db24f
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-05 10:27:20 -0600

    feat: make ImageInput component draggable and improve layout for better user experience

--- lib/components/ImageInput/ImageInput.tsx
+++ lib/components/ImageInput/ImageInput.tsx
@@ -1,7 +1,7 @@
 import { Photo } from "@mui/icons-material";
 import React from "react";
 import { Button } from "../Button";
-import { FocusSpan, Span } from "../FontFaces";
+import { FocusSpan } from "../FontFaces";
 
 const ImageInput = (
   props: React.DetailedHTMLProps<
@@ -14,6 +14,7 @@ const ImageInput = (
   };
   return (
     <label
+      draggable
       htmlFor="newImage"
       className="p-4 border xl:p-7 xl:border-dashed cursor-pointer w-full h-28 xl:h-auto border-black rounded-lg flex items-center justify-center"
     >
@@ -21,10 +22,12 @@ const ImageInput = (
       <Button rounded className="p-2 flex xl:hidden" onClick={onClick}>
         <Photo className="!w-5 !h-5" />
       </Button>
-      <div className="space-y-2 hidden xl:grid gap-2 justify-center items-center">
-        <Photo className="!w-10 !h-10" />
+      <div className="space-y-2 hidden xl:grid gap-2 justify-center text-center items-center">
+        <Photo className="!w-10 !h-10 mx-auto" />
+        {/* TODO: 
         <Span>Drag and drop your images here to upload</Span>
-        <Span>or</Span>
+        <Span>or</Span> 
+        */}
         <Button rounded theme="extra" onClick={onClick}>
           <FocusSpan>Select from your files</FocusSpan>
         </Button>

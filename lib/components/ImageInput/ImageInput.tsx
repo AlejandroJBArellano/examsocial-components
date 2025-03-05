046f5a9761b47a4775a91941f9aeb89f5761b37b
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-05 10:20:56 -0600

    feat: enhance ImageInput component with drag-and-drop support and improved button layout

--- lib/components/ImageInput/ImageInput.tsx
+++ lib/components/ImageInput/ImageInput.tsx
@@ -1,6 +1,7 @@
 import { Photo } from "@mui/icons-material";
 import React from "react";
 import { Button } from "../Button";
+import { FocusSpan, Span } from "../FontFaces";
 
 const ImageInput = (
   props: React.DetailedHTMLProps<
@@ -8,19 +9,26 @@ const ImageInput = (
     HTMLInputElement
   >,
 ) => {
+  const onClick = () => {
+    document.getElementById("newImage")?.click();
+  };
   return (
     <label
       htmlFor="newImage"
-      className="p-4 border cursor-pointer w-full h-28 border-black rounded-lg flex items-center justify-center"
+      className="p-4 border xl:p-7 xl:border-dashed cursor-pointer w-full h-28 xl:h-auto border-black rounded-lg flex items-center justify-center"
     >
       <input type="file" id="newImage" className="hidden" {...props} />
-      <Button
-        rounded
-        className="p-2 flex"
-        onClick={() => document.getElementById("newImage")?.click()}
-      >
+      <Button rounded className="p-2 flex xl:hidden" onClick={onClick}>
         <Photo className="!w-5 !h-5" />
       </Button>
+      <div className="space-y-2 hidden xl:grid gap-2 justify-center items-center">
+        <Photo className="!w-10 !h-10" />
+        <Span>Drag and drop your images here to upload</Span>
+        <Span>or</Span>
+        <Button rounded theme="extra" onClick={onClick}>
+          <FocusSpan>Select from your files</FocusSpan>
+        </Button>
+      </div>
     </label>
   );
 };

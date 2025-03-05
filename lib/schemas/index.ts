145cf3251c67b3e488447ebc8acb128e452d87c6
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-05 12:17:39 -0600

    feat: rename answers to options in questionSchema for clarity and consistency

--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -3,19 +3,19 @@ import { FeedbackCondition } from "../constants";
 
 export const questionSchema = Yup.object({
   question: Yup.string().required("Question is required"),
-  answers: Yup.array()
+  options: Yup.array()
     .of(
       Yup.object({
-        text: Yup.string().required("Answer text is required"),
+        text: Yup.string().required("Option text is required"),
         correct: Yup.boolean(),
       }),
     )
     .required()
-    .min(1, "At least one answer is required")
+    .min(1, "At least one option is required")
     .test(
       "at-least-one-correct",
-      "At least one answer must be correct",
-      (answers) => answers?.some((answer) => answer.correct),
+      "At least one option must be correct",
+      (options) => options?.some((option) => option.correct),
     ),
 });
 

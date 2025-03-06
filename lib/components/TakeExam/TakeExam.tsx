4a415264bb1ca95266caae891a4315ffa426bfc1
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-06 11:59:54 -0600

    refactor: add timer functionality to TakeExam component based on exam settings

--- lib/components/TakeExam/TakeExam.tsx
+++ lib/components/TakeExam/TakeExam.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as Yup from "yup";
 import { examSchema } from "../../schemas";
 import { Stepper } from "../Stepper";
@@ -6,6 +6,33 @@ import SelectedQuestion from "./SelectedQuestion";
 
 const TakeExam = ({ exam }: { exam: Yup.InferType<typeof examSchema> }) => {
   const [selectedQuestion, setSelectedQuestion] = useState(0);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const { setting, hours, minutes, seconds } = exam.advancedSettings.timing;
+    let totalTime = 0;
+
+    if (setting === "TOTAL") {
+      totalTime = (hours || 0) * 3600 + (minutes || 0) * 60;
+    } else if (setting === "PER_QUESTION") {
+      totalTime = (minutes || 0) * 60 + (seconds || 0);
+    }
+
+    setTime(totalTime);
+
+    const timer = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime <= 0) {
+          clearInterval(timer);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [exam.advancedSettings.timing]);
+
   return (
     <main>
       <Stepper
@@ -17,7 +44,7 @@ const TakeExam = ({ exam }: { exam: Yup.InferType<typeof examSchema> }) => {
         steps={exam.questions.length}
         title="Take Exam"
         showDivision
-        time={"60"}
+        time={new Date(time * 1000).toISOString().substr(11, 8)}
       />
       <SelectedQuestion
         selected={selectedQuestion}

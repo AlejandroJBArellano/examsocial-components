1a331e0e3cd31c516d565b9a28067e28245ef27a
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-05 19:18:34 -0600

    refactor: add new theme options to ThemeSettings for enhanced customization

--- lib/components/ThemeSettings/ThemeSettings.tsx
+++ lib/components/ThemeSettings/ThemeSettings.tsx
@@ -6,6 +6,9 @@ export type ThemeSetting = "WHITEBOARD";
 
 const ThemeSettingsNameMap = {
   WHITEBOARD: "Whiteboard",
+  INDUSTRIAL_EDGE: "Industrial Edge",
+  EARTHY_TONES: "Earthy Tones",
+  VIBRANT_ORCHID: "Vibrant Orchid",
 };
 
 interface IThemeSettingsProps {
@@ -20,19 +23,27 @@ const ThemeSettings = ({ onChange }: IThemeSettingsProps) => {
         <FocusSpan>Theme</FocusSpan>
         <div className="w-1/2">
           <Select text={ThemeSettingsNameMap[themeSetting]}>
-            <Select.Option
-              onClick={() => {
-                setThemeSetting("WHITEBOARD");
-                onChange("WHITEBOARD");
-              }}
-              checked={themeSetting === "WHITEBOARD"}
-            >
-              Whiteboard
-            </Select.Option>
+            {Object.entries(ThemeSettingsNameMap).map(([key, value]) => (
+              <Select.Option
+                key={key}
+                onClick={() => {
+                  setThemeSetting(key as ThemeSetting);
+                  onChange(key as ThemeSetting);
+                }}
+                checked={themeSetting === key}
+              >
+                {value}
+              </Select.Option>
+            ))}
           </Select>
         </div>
       </article>
-      <article className="flex items-center gap-4">
+      <article
+        className={
+          "flex items-center gap-4 " +
+          (themeSetting === "WHITEBOARD" ? "" : themeSetting)
+        }
+      >
         <div className="rounded-md border border-black bg-primary size-[77.5px]" />
         <div className="rounded-md border border-black bg-secondary size-[77.5px]" />
         <div className="rounded-md border border-black bg-accent size-[77.5px]" />

06710f292c5ae2c463526330f7be846f5141f385
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-08 11:24:56 -0600

    docs: add documentation to ExamDetail Rating component stories

--- lib/components/ExamDetail/Rating.stories.tsx
+++ lib/components/ExamDetail/Rating.stories.tsx
@@ -5,6 +5,16 @@ import Rating from "./Rating";
 export default {
   title: "Components/ExamDetail/Rating",
   component: Rating,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component:
+          "Rating is a component used in the ExamDetail section to display a numerical rating with a star icon.",
+      },
+    },
+  },
+  tags: ["autodocs"],
 } as Meta;
 
 type Story = StoryObj<typeof Rating>;

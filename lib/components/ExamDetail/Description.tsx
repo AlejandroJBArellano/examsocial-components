34c45b65b102f3768b75e2e2c2aa0a501665a9a3
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-08 10:49:19 -0600

    refactor: reemplazar íconos de @mui por componente Icon en Description.tsx

--- lib/components/ExamDetail/Description.tsx
+++ lib/components/ExamDetail/Description.tsx
@@ -1,21 +1,21 @@
-import { Bookmark, Favorite } from "@mui/icons-material";
 import { Button } from "../Button";
 import { FocusSpan, Paragraph } from "../FontFaces";
+import { Icon } from "../Icon";
 
 const ExamDescription = () => {
   return (
-    <div className="py-6 px-4 space-y-6 bg-primary-tint">
+    <div className="space-y-6 bg-primary-tint px-4 py-6">
       <Paragraph>
         Welcome to the Svelte 5 Quiz! This quiz is designed to test your
         knowledge and understanding of Svelte
       </Paragraph>
       <div className="flex justify-between">
         <div className="flex gap-3">
-          <Button theme="light" className="p-2 bg-transparent">
-            <Favorite className="!size-5" width={20} height={20} />
+          <Button theme="light" className="bg-transparent p-2">
+            <Icon name="favorite" className="!size-5" />
           </Button>
-          <Button theme="light" className="p-2 bg-transparent">
-            <Bookmark className="!size-5" width={20} height={20} />
+          <Button theme="light" className="bg-transparent p-2">
+            <Icon name="bookmark" className="!size-5" />
           </Button>
         </div>
         <Button theme="accent">

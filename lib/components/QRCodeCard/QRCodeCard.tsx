a5df3b71c96cf01b3de9a2a84bf836a348a206c1
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-08 10:47:58 -0600

    refactor: reemplazar íconos de @mui por componente Icon en QRCodeCard.tsx

--- lib/components/QRCodeCard/QRCodeCard.tsx
+++ lib/components/QRCodeCard/QRCodeCard.tsx
@@ -1,24 +1,24 @@
-import { Download, Share } from "@mui/icons-material";
 import { Button } from "../Button";
 import { FocusSpan, Heading3 } from "../FontFaces";
+import { Icon } from "../Icon";
 import { QRCode } from "../QRCode";
 
 const QRCodeCard = () => {
   return (
-    <div className="rounded-lg p-8 border-zinc-300 border flex flex-col justify-between bg-neutral-50">
+    <div className="flex flex-col justify-between rounded-lg border border-zinc-300 bg-neutral-50 p-8">
       <Heading3>Need a QR code?</Heading3>
       <QRCode bgColor="#FAFAFA" quietZone={0} size={446} />
-      <div className="flex justify-between items-center">
+      <div className="flex items-center justify-between">
         <Button
           theme="extra"
           rounded
           className="flex items-center justify-center gap-2"
         >
-          <Share className="!size-5" />
+          <Icon name="share" className="!size-5" />
           <FocusSpan>Share</FocusSpan>
         </Button>
         <Button rounded className="flex items-center justify-between gap-2">
-          <Download className="!size-5" />
+          <Icon name="download" className="!size-5" />
           <FocusSpan>Download</FocusSpan>
         </Button>
       </div>

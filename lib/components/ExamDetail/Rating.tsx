4f8213fa44d1a4d75f3b8741bbac0b8579c55d40
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-08 10:49:00 -0600

    refactor: reemplazar íconos de @mui por componente Icon en Rating.tsx

--- lib/components/ExamDetail/Rating.tsx
+++ lib/components/ExamDetail/Rating.tsx
@@ -1,10 +1,10 @@
-import { Grade } from "@mui/icons-material";
 import { FocusDisplay, Heading3 } from "../FontFaces";
+import { Icon } from "../Icon";
 
 const Rating = ({ count }: { count: number }) => (
-  <div className="rounded-lg p-8 border border-black bg-extra-tint space-y-5">
+  <div className="space-y-5 rounded-lg border border-black bg-extra-tint p-8">
     <div className="flex gap-4">
-      <Grade className="!size-16 text-extra" />
+      <Icon name="grade" className="!size-16 text-extra" />
       <FocusDisplay>{count}</FocusDisplay>
     </div>
     <Heading3 className="ml-20">out of 5 stars</Heading3>

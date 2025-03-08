0678fc001fd0ada8e253c97dca0a87971d106975
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-08 11:24:21 -0600

    docs: add documentation to ExamDetail Header component stories

--- lib/components/ExamDetail/Header.stories.tsx
+++ lib/components/ExamDetail/Header.stories.tsx
@@ -5,6 +5,16 @@ import Header from "./Header";
 export default {
   title: "Components/ExamDetail/Header",
   component: Header,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component:
+          "Header is a component used in the ExamDetail section to display rating information with a star icon.",
+      },
+    },
+  },
+  tags: ["autodocs"],
 } as Meta;
 
 type Story = StoryObj<typeof Header>;

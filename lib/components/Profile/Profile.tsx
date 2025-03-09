69912e478c6435461e0ae88893d7ac37a7270dda
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-09 12:32:07 -0600

    feat: add group hover effect to Profile component button

--- lib/components/Profile/Profile.tsx
+++ lib/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {
 } from "../ProfilePlaceholder";
 const Profile = (props: ProfilePlaceholderProps) => {
   return (
-    <button className="border-x-sm border-gray-600 px-4 hover:border-black md:px-6 md:py-1 xl:px-7 xl:py-5 2xl:px-8 2xl:py-6">
+    <button className="group border-x-sm border-gray-600 px-4 hover:border-black md:px-6 md:py-1 xl:px-7 xl:py-5 2xl:px-8 2xl:py-6">
       <ProfilePlaceholder
         {...props}
         className="size-9 md:size-11 xl:size-12 2xl:size-13"

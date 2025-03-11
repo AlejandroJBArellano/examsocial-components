2a66e9ea53f0555ef3aa5f071fad2a1499461c6a
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-11 10:05:43 -0600

    refactor: enhance Back component with improved type definitions and dynamic classnames

--- lib/components/Back/Back.tsx
+++ lib/components/Back/Back.tsx
@@ -1,10 +1,22 @@
-import { PropsWithChildren } from "react";
+import { cn } from "@/utils";
 import { FocusSpan } from "../FontFaces";
 import { Icon } from "../Icon";
 
-const Back = ({ children }: PropsWithChildren) => {
+const Back = ({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>) => {
   return (
-    <a className="inline-flex items-center justify-start gap-1 self-stretch text-accent-shadow xl:gap-2">
+    <a
+      {...props}
+      className={cn(
+        "inline-flex cursor-pointer items-center justify-start gap-1 self-stretch text-accent-shadow xl:gap-2",
+        props.className,
+      )}
+    >
       <Icon name="arrow_back" size={24} />
       <FocusSpan>{children}</FocusSpan>
     </a>

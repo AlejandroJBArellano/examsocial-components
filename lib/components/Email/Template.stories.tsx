6c2e5de77ba61526d1e02c407eec121d3842efd6
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-11 11:40:00 -0600

    feat: add Email Template Storybook documentation and comprehensive test suite

--- lib/components/Email/Template.stories.tsx
+++ lib/components/Email/Template.stories.tsx
@@ -4,10 +4,26 @@ import EmailTemplate from "./Template";
 export default {
   title: "Components/EmailTemplate",
   component: EmailTemplate,
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      description: {
+        component:
+          "A reusable email template component for the ExamSocial platform. This template provides consistent styling and branding for all transactional emails.",
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    // Add additional argTypes if parameters are added to the component
+  },
 } as Meta<typeof EmailTemplate>;
 
 type Story = StoryObj<typeof EmailTemplate>;
 
+/**
+ * The default email template with standard branding elements.
+ */
 export const Default: Story = {
   args: {},
 };

f28ec92767509289109d277023ba52dc1cb246d9
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-11 14:46:55 -0600

    feat: add responsive padding to IconButton component

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -55,7 +55,9 @@ const IconButton = (props: IconButtonProps) => {
   return (
     <Button
       {...props}
-      className={"flex items-center justify-center p-2 " + props.className}
+      className={
+        "flex items-center justify-center p-2 xl:p-2 " + props.className
+      }
     >
       <Icon
         name={props.children as string}

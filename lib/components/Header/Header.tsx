936cfd4821b094dcbffd1eae0ee2fa1117fa1ce8
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-15 11:50:04 -0600

    feat: enhance Header component to support optional logo link for improved navigation

--- lib/components/Header/Header.tsx
+++ lib/components/Header/Header.tsx
@@ -1,10 +1,20 @@
 import Logo from "@/illustrations/logo";
 import { PropsWithChildren } from "react";
 
-const Header = ({ children }: PropsWithChildren) => {
+interface HeaderProps extends PropsWithChildren {
+  logoHref?: string;
+}
+
+const Header = ({ children, logoHref }: HeaderProps) => {
   return (
     <header className="flex items-center justify-between border-b-sm border-black bg-primary p-4 md:px-6 xl:bg-white xl:px-7 xl:py-5 2xl:px-8 2xl:py-5">
-      <Logo />
+      {logoHref ? (
+        <a href={logoHref}>
+          <Logo />
+        </a>
+      ) : (
+        <Logo />
+      )}
       {children}
     </header>
   );

32624663446f36610633e8068a1ffba451058974
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-15 12:48:01 -0600

    feat: enhance SwitchField component by wrapping children in FocusSpan for improved accessibility and styling

--- lib/components/Field/Switch.tsx
+++ lib/components/Field/Switch.tsx
@@ -1,5 +1,6 @@
 import * as SwitchReact from "@radix-ui/react-switch";
 import { PropsWithChildren, RefAttributes } from "react";
+import { FocusSpan } from "../FontFaces";
 import { Switch } from "../Switch";
 
 export const SwitchField = ({
@@ -9,8 +10,8 @@ export const SwitchField = ({
   Partial<SwitchReact.SwitchProps & RefAttributes<HTMLButtonElement>>
 >) => {
   return (
-    <div className="flex justify-between xl:text-lg">
-      {children}
+    <div className="flex items-center justify-between xl:text-lg">
+      <FocusSpan>{children}</FocusSpan>
       <Switch {...props} />
     </div>
   );
--- lib/components/Switch/Switch.tsx
+++ lib/components/Switch/Switch.tsx
@@ -9,8 +9,8 @@ const Switch = (
   <SwitchReact.Root
     {...props}
     className={cn(
+      "flex w-20 min-w-20 rounded-full border border-black p-1 transition-all duration-300 ease-in-out data-[state=checked]:justify-end data-[state=checked]:bg-primary",
       props.className,
-      "flex rounded-full border border-black p-1 transition-all duration-300 ease-in-out data-[state=checked]:justify-end data-[state=checked]:bg-primary",
     )}
   >
     <SwitchReact.Thumb className="block h-7 w-7 rounded-full bg-black xl:h-8 xl:w-8" />

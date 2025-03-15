06b30698787e6f305e2a7a1ed58d84c2e3811021
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-15 11:48:59 -0600

    feat: update Header component styles to include background color changes for improved visual hierarchy

--- lib/components/Header/Header.tsx
+++ lib/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 
 const Header = ({ children }: PropsWithChildren) => {
   return (
-    <header className="flex items-center justify-between border-b-sm border-black p-4 md:px-6 xl:px-7 xl:py-5 2xl:px-8 2xl:py-5">
+    <header className="flex items-center justify-between border-b-sm border-black bg-primary p-4 md:px-6 xl:bg-white xl:px-7 xl:py-5 2xl:px-8 2xl:py-5">
       <Logo />
       {children}
     </header>

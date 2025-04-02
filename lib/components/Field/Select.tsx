2f091e0111dfebacc248295dc360ea67ca8b4cf6
Alejandro Arellano <jesusarellano334@gmail.com>
2025-04-02 15:46:54 -0600

    feat: introduce SelectField component to enhance form functionality with label, error handling, and helper text support

---
+++ lib/components/Field/Select.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import { FocusSpan, Smoll } from "../FontFaces";
+import { Helper } from "../Helper";
+import { Select, SelectProps } from "../Select";
+
+export interface SelectFieldProps {
+  label: ReactNode;
+  error?: ReactNode;
+  helperText?: ReactNode;
+  selectProps?: SelectProps;
+  children?: ReactNode;
+}
+
+export const SelectField = ({
+  label,
+  error,
+  helperText,
+  selectProps,
+  children,
+}: SelectFieldProps) => {
+  return (
+    <article className="space-y-1">
+      <label className="flex items-center justify-between">
+        <FocusSpan>{label}</FocusSpan>
+        <Helper side="top" align="center">
+          {helperText}
+        </Helper>
+      </label>
+      <div className={`rounded-md ${error ? "bg-feedback-error-tint" : ""}`}>
+        <Select {...selectProps} text={label}>
+          {children}
+        </Select>
+        {error && (
+          <Smoll className="px-2 py-1 text-feedback-error">{error}</Smoll>
+        )}
+      </div>
+    </article>
+  );
+};

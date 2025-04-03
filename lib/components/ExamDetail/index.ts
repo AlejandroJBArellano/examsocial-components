4c993f1a21b5dc35bcd74df355892d02d6ed098e
Alejandro Arellano <jesusarellano334@gmail.com>
2025-04-03 14:17:31 -0600

    feat: add ExamDetail component to export various subcomponents for better modularity and organization

---
+++ lib/components/ExamDetail/index.ts
@@ -0,0 +1,14 @@
+export * from "./Banner";
+export { default as Banner } from "./Banner";
+export * from "./Description";
+export { default as Description } from "./Description";
+export * from "./Header";
+export { default as Header } from "./Header";
+export * from "./QRCodeCard";
+export { default as QRCodeCard } from "./QRCodeCard";
+export * from "./Rating";
+export { default as Rating } from "./Rating";
+export * from "./RetentionRate";
+export { default as RetentionRate } from "./RetentionRate";
+export * from "./UsersCount";
+export { default as UsersCount } from "./UsersCount";

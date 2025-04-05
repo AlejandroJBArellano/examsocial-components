e5fc7874d1f56056dbe94d2e9bdff38498d970f1
Alejandro Arellano <jesusarellano334@gmail.com>
2025-04-05 00:22:18 -0600

    feat: update Input and Textarea components to improve styling and focus states for better user experience

--- lib/components/Input/Input.tsx
+++ lib/components/Input/Input.tsx
@@ -25,17 +25,17 @@ const Input = ({
         {...props}
         className={
           cn(
-            "bg-light peer rounded-md border border-black px-4 py-3 shadow-right-sm transition-all duration-300 ease-in-out",
+            "peer rounded-md border border-black bg-light px-4 py-3 shadow-right-sm transition-all duration-300 ease-in-out",
             "hover:border-black hover:shadow-black",
-            "placeholder:text-zinc-700 placeholder-shown:shadow-none placeholder-shown:hover:shadow-right-sm placeholder-shown:focus:shadow-right-sm placeholder-shown:focus:shadow-extra",
-            "focus:border-extra focus:shadow-right-sm focus:shadow-extra focus:outline-none",
+            "placeholder:text-zinc-700 placeholder-shown:shadow-none placeholder-shown:hover:shadow-right-sm placeholder-shown:focus:shadow-right-sm placeholder-shown:focus:shadow-primary",
+            "focus:border-primary focus:shadow-right-sm focus:shadow-primary focus:outline-none",
             "xl:px-4 xl:py-2 xl:text-lg xl:placeholder:text-lg",
             "hover:shadow-right",
             {
               "border-feedback-error bg-feedback-error-tint text-feedback-error shadow-right-sm shadow-feedback-error":
                 error,
               "hover:border-feedback-error hover:!shadow-feedback-error": error,
-              "placeholder-shown:focus:bg-light placeholder:text-feedback-error":
+              "placeholder:text-feedback-error placeholder-shown:focus:bg-light":
                 error,
               "focus:border-feedback-error focus:text-black focus:shadow-feedback-error":
                 error,
@@ -50,7 +50,7 @@ const Input = ({
         <div
           data-testid="left-icon"
           className={cn(
-            "pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 peer-focus:text-extra",
+            "pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 peer-focus:text-primary",
             {
               "!text-feedback-error": error,
             },
@@ -63,7 +63,7 @@ const Input = ({
         <div
           data-testid="right-icon"
           className={cn(
-            "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 peer-focus:text-extra",
+            "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 peer-focus:text-primary",
             {
               "!text-feedback-error": error,
             },
--- lib/components/Textarea/Textarea.tsx
+++ lib/components/Textarea/Textarea.tsx
@@ -11,17 +11,17 @@ const Textarea = ({ error, ...props }: TextareaProps) => {
       {...props}
       className={
         cn(
-          "bg-light peer rounded-md border border-black px-4 py-3 shadow-right-sm transition-all duration-300 ease-in-out",
+          "peer rounded-md border border-black bg-light px-4 py-3 shadow-right-sm transition-all duration-300 ease-in-out",
           "hover:border-black hover:shadow-black",
-          "placeholder:text-zinc-700 placeholder-shown:shadow-none placeholder-shown:hover:shadow-right-sm placeholder-shown:focus:shadow-right-sm placeholder-shown:focus:shadow-extra",
-          "focus:border-extra focus:shadow-right-sm focus:shadow-extra focus:outline-none",
+          "placeholder:text-zinc-700 placeholder-shown:shadow-none placeholder-shown:hover:shadow-right-sm placeholder-shown:focus:shadow-right-sm placeholder-shown:focus:shadow-primary",
+          "focus:border-primary focus:shadow-right-sm focus:shadow-primary focus:outline-none",
           "xl:px-4 xl:py-2 xl:text-lg xl:placeholder:text-lg",
           "hover:shadow-right",
           {
             "border-feedback-error bg-feedback-error-tint text-feedback-error shadow-right-sm shadow-feedback-error":
               error,
             "hover:border-feedback-error hover:!shadow-feedback-error": error,
-            "placeholder-shown:focus:bg-light placeholder:text-feedback-error":
+            "placeholder:text-feedback-error placeholder-shown:focus:bg-light":
               error,
             "focus:border-feedback-error focus:text-black focus:shadow-feedback-error":
               error,

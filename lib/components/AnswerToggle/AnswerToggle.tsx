34200a027850780dc2a0fc2ee4e24f4d642a72d5
Alejandro Arellano <jesusarellano334@gmail.com>
2025-04-10 11:24:53 -0600

    refactor: adjust AnswerToggle component styles for improved responsiveness and consistency

--- lib/components/AnswerToggle/AnswerToggle.tsx
+++ lib/components/AnswerToggle/AnswerToggle.tsx
@@ -7,7 +7,7 @@ const AnswerToggle = ({ className, ...props }: Checkbox.CheckboxProps) => {
     <Checkbox.Root
       {...props}
       className={cn(
-        "flex aspect-square h-10 w-10 items-center justify-center rounded-md border border-black p-2 transition-all duration-300 ease-in-out xl:h-11 xl:w-11",
+        "flex aspect-square h-8 w-8 items-center justify-center rounded-md border border-black p-[7px] transition-all duration-300 ease-in-out xl:h-10 xl:w-10 xl:p-2",
         "data-[state=indeterminate]:shadow-black data-[state=unchecked]:shadow-black",
         "data-[state=checked]:border-feedback-success data-[state=checked]:bg-feedback-success-tint data-[state=checked]:text-feedback-success data-[state=checked]:shadow-feedback-success",
         "hover:shadow-right-sm",
@@ -15,7 +15,14 @@ const AnswerToggle = ({ className, ...props }: Checkbox.CheckboxProps) => {
       )}
     >
       <Checkbox.Indicator className="flex h-5 w-5 place-items-center xl:h-6 xl:w-6">
-        <Icon name="check" data-testid="CheckIcon" size={20} />
+        <Icon
+          name="check"
+          data-testid="CheckIcon"
+          size={20}
+          responsiveSizes={{
+            xl: 24,
+          }}
+        />
       </Checkbox.Indicator>
     </Checkbox.Root>
   );

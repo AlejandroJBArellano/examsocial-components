50c7e989c214b2164cf4581eb83b157932c798ff
Alejandro Arellano <jesusarellano334@gmail.com>
2025-04-10 19:57:07 -0600

    fix: update border class in Profile component for consistent styling

--- lib/components/Profile/Profile.tsx
+++ lib/components/Profile/Profile.tsx
@@ -9,7 +9,7 @@ const Profile = (props: ProfilePlaceholderProps) => {
 
   return (
     <button
-      className="group border-x-sm border-gray-600 px-4 hover:border-black md:px-6 md:py-1 xl:px-7 xl:py-5 2xl:px-8 2xl:py-6"
+      className="group border-x border-gray-600 px-4 hover:border-black md:px-6 md:py-1 xl:px-7 xl:py-5 2xl:px-8 2xl:py-6"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >

c1549e0e0907358b0095d0e50650cb77ec4bd844
Alejandro Arellano <jesusarellano334@gmail.com>
2025-05-24 18:18:44 -0600

    refactor: remove unused feedback overlap validation functions in AdvancedSettings component

--- lib/components/CreateExam/index.ts
+++ lib/components/CreateExam/index.ts
@@ -1,1 +1,6 @@
+export { AdvancedSettings } from "./AdvancedSettings";
 export { default as CreateExam } from "./CreateExam";
+export { GeneralDetails } from "./GeneralDetails";
+export * from "./PrivacySettings";
+export * from "./ThemeSettings";
+export * from "./TimingSettings";

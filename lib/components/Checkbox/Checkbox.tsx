c0b9f410c6e57e84d7104405618a50cf87fac4b1
Alejandro Arellano <jesusarellano334@gmail.com>
2026-03-16 14:44:10 -0600

    fix: streamline className construction in Checkbox component

--- lib/components/Checkbox/Checkbox.tsx
+++ lib/components/Checkbox/Checkbox.tsx
@@ -8,17 +8,16 @@ export const Checkbox = ({ className, ...props }: CheckboxProps) => {
   return (
     <CheckboxElement.Root
       {...props}
-      className={
-        cn(
-          "flex aspect-square h-6 w-6 items-center justify-center rounded-md border-black text-primary-tint shadow-black transition-shadow duration-300 ease-in-out",
-          "data-[state=checked]:bg-primary data-[state=checked]:disabled:bg-primary-shadow",
-          "hover:shadow-right-sm",
-          "data-[state=checked]:hover:bg-primary-shadow",
-          "disabled:cursor-not-allowed disabled:border-gray-800 disabled:bg-gray-200 disabled:hover:shadow-none",
-          "xl:h-8 xl:w-8",
-          className,
-        ) + " border"
-      }
+      className={cn(
+        "flex aspect-square h-6 w-6 items-center justify-center rounded-md border-black text-primary-tint shadow-black transition-shadow duration-300 ease-in-out",
+        "data-[state=checked]:bg-primary data-[state=checked]:disabled:bg-primary-shadow",
+        "hover:shadow-right-sm",
+        "data-[state=checked]:hover:bg-primary-shadow",
+        "disabled:cursor-not-allowed disabled:border-gray-800 disabled:bg-gray-200 disabled:hover:shadow-none",
+        "xl:h-8 xl:w-8",
+        "border",
+        className,
+      )}
     >
       <CheckboxElement.Indicator className="flex place-items-center text-light">
         <Icon
